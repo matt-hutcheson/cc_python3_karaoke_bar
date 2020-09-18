171bd8950e34a6114b441914a9555bec48ef64d1
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-18 18:06:09 +0100

    added check_out_guest_from_room with multiple guests test and passed

--- tests/test_room.py
+++ tests/test_room.py
@@ -7,6 +7,7 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room_tropical = Room("Tropical Room")
         self.room_magma = Room("Magma")
+        self.test_guest = Guest("Party Bob", 38, 2000.00)
 
     def test_room_exists(self):
         self.assertEqual("Tropical Room", self.room_tropical.name)
@@ -14,12 +15,21 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(0, len(self.room_tropical.song_list))
 
     def test_check_in_guest_to_room(self):
-        test_guest = Guest("Party Bob", 38, 2000.00)
-        self.room_tropical.check_in_guest(test_guest)
+        self.room_tropical.check_in_guest(self.test_guest)
         self.assertEqual(1, len(self.room_tropical.guest_list))
 
-    def test_check_out_guest_to_room(self):
-        test_guest = Guest("Party Bob", 38, 2000.00)
-        self.room_tropical.check_in_guest(test_guest)
-        self.room_tropical.check_out_guest(test_guest)
+    def test_check_out_guest_from_room(self):
+        self.room_tropical.check_in_guest(self.test_guest)
+        self.room_tropical.check_out_guest(self.test_guest)
         self.assertEqual(0, len(self.room_tropical.guest_list))
+
+    def test_check_out_guest_from_room_multiple_guests(self):
+        test_guest2 = Guest("Musical Bob", 54, 700.00)
+        test_guest3 = Guest("Tone Deaf Bob", 76, 500.00)
+        self.room_tropical.check_in_guest(self.test_guest)
+        self.room_tropical.check_in_guest(test_guest2)
+        self.room_tropical.check_in_guest(test_guest3)
+        self.room_tropical.check_out_guest(test_guest2)
+        self.assertEqual("Party Bob", self.room_tropical.guest_list[0].name)
+        self.assertEqual("Tone Deaf Bob", self.room_tropical.guest_list[1].name)
+        self.assertEqual(2, len(self.room_tropical.guest_list))
