f57a06fb0c7d3f3270d9c9bbde41a537d6152b73
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-18 16:17:03 +0100

    added test_guest_exists test and passed

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tests.test_guests import Guests
+from tests.test_guests import TestGuests
 # from tests.test_rooms import Rooms
 # from tests.test_songs import Songs
 
--- tests/test_guests.py
+++ tests/test_guests.py
@@ -2,6 +2,14 @@ import unittest
 
 from src.guests import Guests
 
-class TestGuests():
+class TestGuests(unittest.TestCase):
     def setUp(self):
-        pass
+        self.guest_loud_bob = Guests("Loud Bob", 23, 200.00)
+        self.guest_shy_bob = Guests("Shy Bob", 18, 1000.00)
+
+    def test_guest_exists(self):
+        self.assertEqual("Loud Bob", self.guest_loud_bob.name)
+        self.assertEqual(23, self.guest_loud_bob.age)
+        self.assertEqual(200.00, self.guest_loud_bob.wallet)
+
+    
