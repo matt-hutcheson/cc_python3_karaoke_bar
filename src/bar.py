0d3550f97b256fa3afa071333aa9f3a962bc5313
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-20 13:59:02 +0100

    changed drinks list to drinks dictionary and modified bar functions to pass tests

--- src/bar.py
+++ src/bar.py
@@ -2,17 +2,24 @@ class Bar:
     def __init__(self, name, till):
         self.name = name
         self.till = till
-        self.drinks = []
+        self.drinks = {}
         self.rooms = []
 
     def add_money_to_till(self, money_to_add):
         self.till += money_to_add
 
     def add_drink(self, drink):
-        self.drinks.append(drink)
+        if drink.name in self.drinks:
+            self.drinks[drink.name]["stock"] += 1
+        else:
+            self.drinks[drink.name] = {"price":drink.price, "alcohol_units":drink.alcohol_units, "stock": 1}
 
     def remove_drink(self, drink):
-        self.drinks.remove(drink)
+        if drink.name in self.drinks:
+            self.drinks[drink.name]["stock"] -= 1
 
     def stock_count(self):
-        return len(self.drinks)
+        stock_count = 0
+        for drink in self.drinks:
+            stock_count += self.drinks[drink]["stock"]
+        return stock_count
--- tests/bar_test.py
+++ tests/bar_test.py
@@ -8,7 +8,14 @@ class TestBar(unittest.TestCase):
         self.bar = Bar("Music and Spirits", 100.00)
         self.beer = Drink("Tennents", 4.00, 2.00)
         self.cocktail = Drink("Depth Charge",8.50, 3.00)
-        self.bar.drinks = [self.beer, self.cocktail]
+        self.bar.drinks = {
+            "Tennents":{
+                "price":4.00, "alcohol_units":2.00, "stock":1
+            },
+            "Depth Charge":{
+                "price":8.50, "alcohol_units":3.00, "stock":1
+            }
+        }
 
     def test_bar_has_name(self):
         self.assertEqual("Music and Spirits", self.bar.name)
@@ -17,7 +24,7 @@ class TestBar(unittest.TestCase):
         self.assertEqual(100.00, self.bar.till)
 
     def test_bar_has_drinks(self):
-        self.assertEqual(2,len(self.bar.drinks))
+        self.assertEqual(2,self.bar.stock_count())
     
     def test_add_money_to_till(self):
         money_to_add = self.beer.price
@@ -26,11 +33,11 @@ class TestBar(unittest.TestCase):
 
     def test_add_drink(self):
         self.bar.add_drink(self.beer)
-        self.assertEqual(3, len(self.bar.drinks))
+        self.assertEqual(3, self.bar.stock_count())
 
     def test_remove_drink(self):
         self.bar.remove_drink(self.beer)
-        self.assertEqual(1, len(self.bar.drinks))
+        self.assertEqual(1, self.bar.stock_count())
 
     def test_stock_count(self):
         self.assertEqual(2, self.bar.stock_count())
